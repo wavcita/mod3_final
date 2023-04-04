d19233678015376272ceef438a3bce703ed5e9c6
wavcita <wavcita@hotmail.com>
2023-04-04 04:43:09 -0400

    borrador2

--- src/pages/components/books.page.ts
+++ src/pages/components/books.page.ts
@@ -9,5 +9,13 @@ const tie=500;
 class Form extends BasePage {
     
     private formButton: string = '//li[@class="btn btn-light active"]'
+    private urlBook: string = '//a[normalize-space()="Git Pocket Guide"]'
+
+    constructor(){
+        super();
+    }
+
+    
+
 
 }
